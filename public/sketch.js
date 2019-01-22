2b4ac5957009cd760677038ae4be8752dd1905fd
yashGoien <43846761+yashGoien@users.noreply.github.com>
2019-01-22 22:19:49 +0530

    Add files via upload

--- public/sketch.js
+++ public/sketch.js
@@ -82,13 +82,6 @@ function mousePressed() {
     for (var j = 0; j < rows; j++) {
       if (grid[i][j].contains(mouseX, mouseY)) {
         if (!grid[i][j].bee) {
-          // do {
-          var questions = [];
-
-          for (var n = 0; n < 75; n++) {
-            questions.push([n]);
-          }
-
           var num = floor(random(questions.length));
           questions.splice(num, 1);
 
@@ -212,3 +205,81 @@ function saveToList() {
 window.onbeforeunload = function() {
   event.preventDefault();
 };
+
+let questions = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  29,
+  30,
+  31,
+  32,
+  33,
+  34,
+  35,
+  36,
+  37,
+  38,
+  39,
+  40,
+  41,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  48,
+  49,
+  50,
+  51,
+  52,
+  53,
+  54,
+  55,
+  56,
+  57,
+  58,
+  59,
+  60,
+  61,
+  62,
+  63,
+  64,
+  65,
+  66,
+  67,
+  68,
+  69,
+  70,
+  71,
+  72,
+  73,
+  74,
+  75
+];
